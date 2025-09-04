3b93f60ba15c5343f48429ee4fe9107cbea9ed62
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-09-04 20:31:49 +0500

    solved container bug

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/container";
+import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Button from "./Button.jsx";

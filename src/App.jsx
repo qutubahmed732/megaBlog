121572208549652db7adc91f9cf7310a9e5fa275
Qutub Ahmed <qutubahmed538@yahoo.com>
2026-09-08 20:14:46 +0500

    refactor: polish app shell and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Header, Footer } from "./Interface/index.js";
-
+import { Header, Footer } from './Interface/index.js';
 import authService from './appwrite/auth.js';
-import { loginUser, logout } from "./Redux-store/features/authSlice.js";
+import { loginUser, logout } from './Redux-store/features/authSlice.js';
 import { Outlet } from 'react-router-dom';
 
-
 function App() {
-
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
-  const imgRef = useRef(null)
 
   useEffect(() => {
     authService.getCurrentUser()
       .then((userData) => {
-        if (userData) {
-          dispatch(loginUser({ userData }));
-        } else {
-          dispatch(logout());
-        }
+        if (userData) dispatch(loginUser({ userData }));
+        else dispatch(logout());
       })
       .finally(() => setLoading(false));
-  }, []);
+  }, [dispatch]);
 
-
-  return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className="w-full block">
-        <Header />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
+  if (loading) {
+    return (
+      <div className="site-loader">
+        <div className="loader-mark">B</div>
+        <p>Loading BlogHouse</p>
       </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col site-shell">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
-  ) : null;
+  );
 }
 
 export default App;
